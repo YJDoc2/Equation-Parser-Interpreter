3d399f38161a2b22fd96e133fd2d50a26b8eac9f
YatharthVyas <yatharth2000.vyas@gmail.com>
2019-11-29 22:05:50 +0530

    Added count of lines and improved while loop

--- [Work in Progress] Yatharth/read.cpp
+++ [Work in Progress] Yatharth/read.cpp
@@ -6,11 +6,18 @@ using namespace std;
 int main(){
 	ifstream fileReader;
 	string line;
+	int countLines=0;
 	fileReader.open("example.txt");
-	while(!fileReader.eof()){						//Loops till we reach end of file
-		getline(fileReader,line);
-		cout<<line<<endl;
+	if (fileReader.bad()||!fileReader){			//Prints Error message if file is empty or doesnt exist
+		cout<<"Invalid file"<<endl;
 	}
+	else{
+		while(getline(fileReader,line)){						//Loops till we reach end of file
+			countLines++;
+			cout<<line<<endl;
+		}
+	}
+	cout<<"Number of Lines: "<<countLines<<endl;
 	fileReader.close();
 	return 0;
 }
